d8445a6c073c54834ea6d285bc01105d255a4509
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-09-17 18:49:36 +0900

    Initial Commit

---
+++ CATechChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CATechChallenge
+//
+//  Created by Sousuke Ikemoto on 2020/09/17.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
