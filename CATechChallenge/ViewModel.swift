85e3b0892f44b9dbd8fe66877f59a032509edcf8
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-10-06 18:44:35 +0900

    フォーマット直し

--- CATechChallenge/ViewModel.swift
+++ CATechChallenge/ViewModel.swift
@@ -9,21 +9,19 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-
-
 final class FeedCellViewModel {
     private let model: CommentModel = .init()
-
+    
     // input
-
+    
     let commentButtonTaped = PublishRelay<Void>()
     let hideTableViewButtonTaped = PublishRelay<Void>()
     let hideFlowCommentButtonTaped = PublishRelay<Void>()
-
+    
     // output
-
+    
     private let fetchedComments = BehaviorRelay<[Comment]>(value: [])
-
+    
     var comments = BehaviorRelay<[Comment]>(value: [])
     var addcomments = BehaviorRelay<Comment>(value: .init(id: "", userId: "", message: "", createdAt: Date(timeIntervalSince1970: 2)))
     
@@ -31,32 +29,32 @@ final class FeedCellViewModel {
     var hideFlowCommentBool = BehaviorRelay<Bool>(value: false)
     
     private let disposebag = DisposeBag()
-
-
+    
+    
     init() {
         commentButtonTaped.throttle(.milliseconds(500), scheduler: MainScheduler.instance)
-        .flatMap{[weak self] ()  -> Observable<[Comment]> in
-            guard let self = self else{return .empty()}
-            return self.model.getCommentMessage()}
-        .do(onNext: { [weak self] comment in self?.getRandomComment()})
-        .bind(to: fetchedComments)
-        .disposed(by:disposebag)
+            .flatMap{[weak self] ()  -> Observable<[Comment]> in
+                guard let self = self else{return .empty()}
+                return self.model.getCommentMessage()}
+            .do(onNext: { [weak self] comment in self?.getRandomComment()})
+            .bind(to: fetchedComments)
+            .disposed(by:disposebag)
         
         hideTableViewButtonTaped.subscribe(onNext: {[weak self] _ in
-            guard let self = self  else {return}
-            self.hideTableViewBool.accept(!self.hideTableViewBool.value)})
+                                            guard let self = self  else { return }
+                                            self.hideTableViewBool.accept(!self.hideTableViewBool.value)})
             .disposed(by: disposebag)
         hideFlowCommentButtonTaped.subscribe(onNext: {[weak self] _ in
-            guard let self = self  else {return}
-            self.hideFlowCommentBool.accept(!self.hideFlowCommentBool.value)})
+                                                guard let self = self  else { return }
+                                                self.hideFlowCommentBool.accept(!self.hideFlowCommentBool.value)})
             .disposed(by: disposebag)
-        }
+    }
     func requestApi(){
         model.getCommentMessage().bind(to: fetchedComments).disposed(by: disposebag)
     }
-
+    
     private func getRandomComment(){
-        guard  let comment = fetchedComments.value.randomElement() else {return}
+        guard  let comment = fetchedComments.value.randomElement() else { return }
         var comments2 = comments.value
         comments2.append(comment)
         addcomments.accept(comment)
